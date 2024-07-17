eb1fecd4602d14421398e68ebeb8c07a61025ec0
Ishit Rastogi <ishitrastogi@gmail.com>
2024-07-17 10:38:46 +0530

    view only

--- src/Achievements.jsx
+++ src/Achievements.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 const Achievements = () => {
-  const url = 'https://docs.google.com/document/d/1OBjUmR6H63C0OiiQuCJ9ICmXIvrXAhN8WxH0uNgU0EA/edit?usp=sharing'
+  const url = 'https://docs.google.com/document/d/1OBjUmR6H63C0OiiQuCJ9ICmXIvrXAhN8WxH0uNgU0EA/edit?usp=drivesdk'
   useEffect(() => {
     window.location.href = url;
  }, [url]);
