396e0d56e8f0d63d82d4760010b244fbee589d5d
Ishit Rastogi <ishitrastogi@gmail.com>
2024-07-31 09:52:09 +0530

    AA

--- src/Amigmun.jsx
+++ src/Amigmun.jsx
@@ -182,7 +182,7 @@ const Amigmun = () => {
             <h1 className='sec' style={{marginTop:'5vw'}}>Gallery</h1>
 
             <div className="video">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/BV46b6M1Hps?si=WC96PVbpSh5XrP8E" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/x1eYbnp_OPw?si=i6jfLKEe6uni3Mxk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
 
             <div className="img">
