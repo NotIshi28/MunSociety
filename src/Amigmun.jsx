ffbfb17d61bfcdda491fd397e0bc6d35380de675
Ishit Rastogi <ishitrastogi@gmail.com>
2025-07-15 20:52:36 +0530

    fix matrix link

--- src/Amigmun.jsx
+++ src/Amigmun.jsx
@@ -188,7 +188,7 @@ const Amigmun = () => {
                     </div>
                     <div className="socials ltext">
                       <div>Whatsapp: <a target="_blank"  href={committee.whatsapp}>Click Here</a> </div>
-                      <div>Matrix: <a target="_blank" href='https://docs.google.com/spreadsheets/d/1xgHD3FujmnzpVQOlsrXiqVfTCRGF-B1RQszLD4CFtIM/edit?usp=sharing'>Click Here</a></div>
+                      <div>Matrix: <a target="_blank" href='https://docs.google.com/spreadsheets/d/1PEVA6-omhAO_24l6qFqwB1W9Q_SXVAZgtFP3TAGbRDw/edit?usp=sharing'>Click Here</a></div>
                       <div>Background Guide: <a target="_blank" href={committee.backgroundGuide}>Click Here</a> </div>
                     </div>
                   </div>
